a2d6dabd39748753f1cae57a16564d7616b6cdbc
Liamm3 <liam.wohlfart0@gmail.com>
2025-12-21 18:59:08 +0100

    split into modules

---
+++ home-manager-modules/cli.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.packages = with pkgs; [
+    wget
+
+    (writeShellScriptBin "nrs" ''
+      sudo nixos-rebuild switch --flake /home/liam/nix-dotfiles#genesis
+    '')
+
+    (writeShellScriptBin "nrt" ''
+      sudo nixos-rebuild test --flake /home/liam/nix-dotfiles#genesis
+    '')
+  ];
+
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Liamm3";
+      email = "liam.wohlfart0@gmail.com";
+    };
+    settings.init.defaultBranch = "main";
+  };
+  programs.gh.enable = true;
+  programs.zsh.enable = true;
+  programs.lazygit = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  programs.btop.enable = true;
+}
