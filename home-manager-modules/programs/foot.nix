aaea825274bdddb14868b61fef1eef251a953754
Liamm3 <liam.wohlfart0@gmail.com>
2026-02-10 22:47:17 +0100

    foot setup

--- home-manager-modules/programs/foot.nix
+++ home-manager-modules/programs/foot.nix
@@ -1,4 +1,13 @@
+# How to add a theme: https://www.reddit.com/r/Nix/comments/18o6698/foot_configuration_in_homemanager_importing_a/
 { ... }:
 {
-  programs.foot.enable = true;
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMonoNerdFont:size=9";
+        dpi-aware = "yes";
+      };
+    };
+  };
 }
