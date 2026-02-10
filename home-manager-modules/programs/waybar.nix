36ef4c4065973c35de05d7ac811235f9904c2f3c
Liamm3 <liam.wohlfart0@gmail.com>
2026-02-10 22:07:11 +0100

    add waybar from arch book

--- home-manager-modules/programs/waybar.nix
+++ home-manager-modules/programs/waybar.nix
@@ -3,23 +3,91 @@
   programs.waybar = {
     enable = true;
     settings.mainBar = {
-      layer = "top";
-      position = "top";
-      height = 30;
       modules-left = [
-        "hyprland/workspaces"
+        "ext/workspaces"
       ];
-      "hyprland/workspaces" = {
-        format = "{name}";
-        format-icons = {
-          urgent = "";
-          active = "";
-          visible = "";
-          default = "";
-          empty = "";
-        };
-        all-outputs = false;
+      modules-center = [
+        "dwl/window"
+      ];
+      modules-right = [
+        "pulseaudio"
+        "network"
+        "temperature"
+        "battery"
+        "clock"
+      ];
+      "ext/workspaces" = {
+        format = "{icon}";
+        on-click = "activate";
+        sort-by-id = true;
+        ignore-hidden = false;
+      };
+      "dwl/tags" = {
+        num-tags = 9;
+      };
+      "dwl/window" = {
+        format = "{title}";
+      };
+      clock = {
+        format = "{:%d.%m.%Y %H:%M}";
+      };
+      network = {
+        format-wifi = "{essid}";
       };
     };
+
+    style = ''
+          * {
+        border: none;
+        border-radius: 0;
+        font-family: monospace;
+        font-size: 14px;
+        min-height: 0;
+      }
+
+      window#waybar {
+        background: #201b14;
+        color: #bbbbbb;
+      }
+
+      #workspaces button {
+        padding: 0 8px;
+        background: transparent;
+        color: #bbbbbb;
+        border-bottom: 3px solid transparent;
+      }
+
+      #workspaces button.active {
+        background: #c9b890;
+        color: #201b14;
+      }
+
+      #workspaces button.urgent {
+        background: #ad401f;
+        color: #eeeeee;
+      }
+
+      #workspaces button:hover {
+        background: #444444;
+        color: #eeeeee;
+      }
+
+      #window {
+        padding: 0 10px;
+        color: #bbbbbb;
+      }
+
+      #pulseaudio,
+      #battery,
+      #network,
+      #cpu,
+      #temperature,
+      #clock {
+        padding: 0 10px;
+        background-color: #282a36; /* Falls du farbige "Pillen" willst */
+        border-radius: 8px;
+        margin-left: 10px;
+      }
+    '';
   };
 }
